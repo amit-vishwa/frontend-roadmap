a96c87de6d2dd004874e3ad07c778042cc07a662
amit-vishwa <amitvishwa789@gmail.com>
2024-02-09 21:38:00 +0530

    created modal window

--- javascript/complete-javascript-course-master/06-Modal/starter/script.js
+++ javascript/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalButton = document.querySelector('.close-modal');
+const showModalButton = document.querySelectorAll('.show-modal');
+// console.log(showModalButton);
+
+const openModal = () => {
+  // all class properties needs to be updated using below lines
+  // modal.style.display = 'inherit';
+  // overlay.style.display = 'inherit';
+
+  // only removing class will handle everything
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+showModalButton.forEach(show => {
+  //   console.log(show.textContent);
+  show.addEventListener('click', openModal);
+});
+
+closeModalButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') closeModal();
+  //   else openModal();
+});
